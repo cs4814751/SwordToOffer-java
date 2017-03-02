1564514eccbd508c68ec5c37b892e9277355c6e7
leibro <cs4814751@gmail.com>
2017-03-02 16:41:59 +0800

    q7 optimized

--- src/main/java/q7/Solution.java
+++ src/main/java/q7/Solution.java
@@ -14,14 +14,12 @@ public class Solution {
     }
 
     public int pop() {
-        while(!stack1.empty()) {
-            stack2.push(stack1.pop());
+        if(stack2.empty()) {
+            while(!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
         }
-        int r = stack2.pop();
-        while(!stack2.empty()) {
-            stack1.push(stack2.pop());
-        }
-        return r;
+        return stack2.pop();
     }
 
 
